13907227a36c5e778db732fbad77f6d4cc82f008
YASH <iamyash337@gmail.com>
2023-01-07 14:40:56 +0530

    main

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -1,82 +1,50 @@
-// ASSIGNMENT NO - 5
-// NAME - YASH JITENDRA CHINCHOLE
-// ROLL NO - SYCOA37
-
 #include <bits/stdc++.h>
 using namespace std;
 
-class shape
+class Shape
 {
 public:
     double length, breadth;
-    shape()
-    {
-    }
+
     void get_data()
     {
-        cout << "enter dimension:" << endl;
+        cout << "Enter Dimensions" << endl;
         cin >> length >> breadth;
     }
-    virtual void area()
-    {
-    }
+    virtual void area() = 0;
 };
-class triangle : public shape
-{
-public:
-    triangle()
-    {
-    }
-    void area()
-    {
-        cout << "the area of triangle is " << (0.5 * length * breadth) << endl;
-    }
-};
-class rectangle : public shape
-{
-public:
-    rectangle()
-    {
-    }
 
+class Triangle : public Shape
+{
+public:
     void area()
     {
-        cout << "the area of rectangle is " << (length * breadth) << endl;
+        cout << "Area of Triangle : " << 0.5 * length * breadth << endl;
     }
 };
+
+class Rectangle : public Shape
+{
+public:
+    void area()
+    {
+        cout << "Area of Rectangle : " << length * breadth << endl;
+    }
+};
+
 int main()
 {
-    shape *s;
-    int choice;
-    while (1)
-    {
-        cout << "menu" << endl
-             << "1)triangle" << endl
-             << "2)rectangle" << endl
-             << "3)exit" << endl;
-        cin >> choice;
-        if (choice == 1)
-        {
-            triangle t;
-            t.get_data();
-            s = &t;
-            s->area();
-        }
-        else if (choice == 2)
-        {
-            rectangle r;
-            r.get_data();
-            s = &r;
-            s->area();
-        }
-        else if (choice == 3)
-        {
-            return 0;
-        }
-        else
-        {
-            cout << "wrong input" << endl;
-        }
-    }
+    Shape *s;
+
+    Triangle t;
+    t.get_data();
+    s = &t;
+    s->area();
+
+    Rectangle r;
+    r.get_data();
+    s = &r;
+    s->area();
+
     return 0;
 }
