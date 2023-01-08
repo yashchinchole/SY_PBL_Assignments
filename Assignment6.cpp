43f3247e18097f3b784d9ccf9a04dd4bbda4a536
YASH <iamyash337@gmail.com>
2023-01-08 16:01:14 +0530

    main

--- Assignment6.cpp
+++ Assignment6.cpp
@@ -4,12 +4,13 @@ using namespace std;
 class Mat
 {
 private:
-    int m[10][10];
+    int mat[10][10];
     int row, col;
 
 public:
     void Read();
     void Add(Mat);
+    void Mul(Mat);
 };
 
 void Mat::Read()
@@ -24,7 +25,6 @@ void Mat::Read()
         if (row > 10 || col > 10)
             throw 2;
     }
-
     catch (int n)
     {
         if (n == 2)
@@ -34,49 +34,86 @@ void Mat::Read()
         }
     }
 
+    cout << "Enter Matrix" << endl;
     for (int i = 0; i < row; i++)
-    {
         for (int j = 0; j < col; j++)
-            cin >> m[i][j];
-    }
+            cin >> mat[i][j];
 }
 
-void Mat::Add(Mat m2)
+void Mat::Add(Mat m)
 {
     try
     {
-        if (this->row != m2.row || this->col != m2.col)
+        if (this->row != m.row || this->col != m.col)
             throw 2;
     }
-
     catch (int n)
     {
         if (n == 2)
         {
-            cout << "Number of Rows & Columns Do Not Match" << endl;
+            cout << "Number of Rows & Columns Are Not Same" << endl;
             return;
         }
     }
 
     int arr[this->row][this->col];
 
-    for (int i = 0; i < this->row; i++)
+    cout << "Addition" << endl;
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 0; j < this->col; j++)
+        for (int j = 0; j < col; j++)
         {
-            arr[i][j] = this->m[i][j] + m2.m[i][j];
-            cout << arr[i][j];
+            arr[i][j] = this->mat[i][j] + m.mat[i][j];
+            cout << arr[i][j] << " ";
         }
+        cout << endl;
+    }
+}
+
+void Mat::Mul(Mat m)
+{
+    try
+    {
+        if (this->row != m.row || this->col != m.col)
+            throw 2;
+    }
+    catch (int n)
+    {
+        if (n == 2)
+        {
+            cout << "Number of Rows & Columns Are Not Same" << endl;
+            return;
+        }
+    }
+
+    int arr[this->row][this->col];
+
+    cout << "Multiplicaion" << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            arr[i][j] = 0;
+            for (int k = 0; k < row; k++)
+                arr[i][j] += this->mat[i][k] * m.mat[k][j];
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
     }
 }
 
 int main()
 {
     Mat m1, m2;
-
     m1.Read();
     m2.Read();
     m1.Add(m2);
+    m1.Mul(m2);
 
     return 0;
 }
